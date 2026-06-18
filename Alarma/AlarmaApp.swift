cbbdbb67c537d44d13ce1aa91649c3b21b256523
fllapp <facundollamedo93@gmail.com>
2026-06-18 11:54:00 -0300

    Remove deprecated UILocalNotification code, rely on UN delegate

--- Alarma/AlarmaApp.swift
+++ Alarma/AlarmaApp.swift
@@ -18,14 +18,6 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         AlarmManager.shared.requestNotificationPermission()
         AlarmManager.shared.setupAudioSession()
-
-        if let localNote = launchOptions?[.localNotification] as? UILocalNotification,
-           let userInfo = localNote.userInfo,
-           let alarmID = userInfo["alarm_id"] as? String,
-           let alarm = AlarmManager.shared.alarms.first(where: { $0.id.uuidString == alarmID }) {
-            AlarmManager.shared.triggerAlarm(alarm)
-        }
-
         return true
     }
 
